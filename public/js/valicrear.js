a1a4be2da22b24396159d21d26b016c50786e8c4
sguzowski <sebaguzowski@gmail.com>
2022-11-27 14:01:42 -0300

    validaciones register crear seba

---
+++ public/js/valicrear.js
@@ -0,0 +1,37 @@
+window.addEventListener("load", function(){
+    let formulario = document.querySelector("form.formcrear");
+    formulario.addEventListener("submit", function(e){
+        e.preventDefault();
+        console.log("holaseba");
+        let errores = [];
+
+        let campoNombre = document.querySelector("input.name");
+        if (campoNombre.value == ""){
+            errores.push("El nombre no puede estar vacio");
+        };
+
+        let campoDescripcion = document.querySelector("input.descripcion");
+        if (campoDescripcion.value == ""){
+            errores.push("La descripcion no puede estar vacia");
+        };
+
+        
+        let campoMarca = document.querySelector("input.marca");
+        if (campoMarca.value == ""){
+            errores.push("La marca no puede estar vacia");
+        };
+
+        let campoPrecio = document.querySelector("input.precio");
+        if (campoPrecio.value == ""){
+            errores.push("El precio no puede estar vacio");
+        };
+
+        if (errores.length > 0){
+            e.preventDefault();
+            let ulError = document.querySelector("div.listaerrores ul");
+            for (let i=0;i<errores.length;i++){
+                ulError.innerHTML += "<li>" + errores[i] + "</li>";
+            }
+        };
+    });
+});
---
+++ public/js/valiregistro.js
@@ -0,0 +1,58 @@
+window.addEventListener("load", function(){
+    let formulario = document.querySelector("form.formregister");
+    formulario.addEventListener("submit", function(e){
+
+        let errores = []; 
+
+        let campoNombre = document.querySelector("input.name");
+        if (campoNombre.value == ""){
+            errores.push("El nombre no puede estar vacio");
+        }else if (campoNombre.value.length < 3){
+            errores.push("El nombre debe tener mas de 3 letras");
+        };
+
+        let campoUsuario = document.querySelector("input.usuario");
+        if (campoUsuario.value == ""){
+            errores.push("El usuario no puede estar vacio");
+        }else if (campoUsuario.value.length < 10){
+            errores.push("El usuario debe tener mas de 10 caracteres");
+        };
+
+        let campoPass = document.querySelector("input.password");
+        if (campoPass.value == ""){
+            errores.push("La contraseña no puede estar vacia");
+        }else if (campoPass.value.length < 6 || campoPass.value.length > 10){
+            errores.push("La contraseña debe tener entre 6 y 10 caracteres");
+        };
+
+        let campoPass2 = document.querySelector("input.password2");
+        if (campoPass2.value != campoPass.value){
+            errores.push("La contraseña no coincide");
+        };
+
+        let campoEmail = document.querySelector("input.email");
+        if (campoEmail.value == ""){
+            errores.push("El email no puede estar vacio")
+        };
+
+        let campoEdad = document.querySelector("input.edad");
+        if (campoEdad.value == ""){
+            errores.push("La edad no puede estar vacia");
+        }else if (parseInt(campoEdad.value) < 18 || parseInt(campoEdad.value) > 99){
+            errores.push("Ingrese una edad valida");
+        };
+
+        let campoTele = document.querySelector("input.telefono");
+        if (campoTele.value == ""){
+            errores.push("El telefono no puede estar vacio");
+        };
+
+        if (errores.length > 0){
+            e.preventDefault();
+            let ulError = document.querySelector("div.listaerrores ul");
+            for (let i = 0; i<errores.length; i++){
+                ulError.innerHTML += "<li>"+ errores[i] +"</li>";
+            }
+        };
+    });
+});
